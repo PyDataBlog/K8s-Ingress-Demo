c213e1511922033eddfc7fef07331e2a358acc8c
PyDataBlog <pimpfada@gmail.com>
2022-06-26 11:57:14 +0200

    Added tls script plus improved codebase

---
+++ enable_tls.sh
@@ -0,0 +1,8 @@
+if kubectl create secret -n kube-system tls mkcert-tls-secret --cert=Secrets/_wildcard.myminikube.demo+2.pem --key=Secrets/_wildcard.myminikube.demo+2-key.pem ; then
+    echo "TLS secret created"
+else
+    echo "TLS secret already exists?"
+fi && \
+minikube addons configure ingress <<< "kube-system/mkcert-tls-secret" && \
+minikube addons disable ingress && \
+minikube addons enable ingress
--- release_hello_world.sh
+++ release_hello_world.sh
@@ -3,3 +3,14 @@ for yaml_file in $(find Namespaces Deployments Services Ingresses -name "hello_*
     kubectl apply -f $yaml_file
 done
 echo "Hello World app deployed on the cluster!"
+
+ip=""
+while [ -z $ip ]; do
+    echo "Waiting for external IP"
+    ip=$(kubectl get ingress -n ingress-demo -o jsonpath="{.items[*].status.loadBalancer.ingress[*].ip}")
+    [ -z "$ip" ] && sleep 5
+done
+
+echo 'Found external Ingress IP'
+url=$(kubectl get ingress -n ingress-demo -o jsonpath="{.items[*].spec.rules[*].host}")
+echo 'Visit https://'$url' in your browser'
